c9f0d940b978c7561fdcae3724df6549089e1e3a
koinhug@126.com <koinhug@126.com>
2016-07-28 22:42:07 +0800

    NodeJS 开始

---
+++ XOrange/NodeJS/NodeJSTest/counter/main.js
@@ -0,0 +1,7 @@
+var counter1 = require('./util/counter');
+var counter2 = require('./util/counter');
+
+// counter.js 并没有因为被 require 两次而初始化两次，而是被缓存起来，重复利用。
+console.log(counter1.count());
+console.log(counter2.count());
+console.log(counter2.count());
