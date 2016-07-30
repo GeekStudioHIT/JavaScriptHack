43d203b6b5d4411bb0941a65cd31b493ac0b990c
koinhug@126.com <koinhug@126.com>
2016-07-30 14:11:28 +0800

    HTTP 搞定

--- XOrange/NodeJS/NodeJSTest/HTTPServerClient/HTTPClient.js
+++ XOrange/NodeJS/NodeJSTest/HTTPServerClient/HTTPClient.js
@@ -11,7 +11,20 @@ var options = {
     }
 };
 
-var request = http.request(options, function (response) {});
+var request = http.request(options, function (response) {
+    var body = [];
+    console.log(response.statusCode);
+    console.log(response.headers);
+
+    response.on('data', function (chunk) {
+        body.push(chunk);
+    });
+
+    response.on('end', function () {
+        body = Buffer.concat(body);
+        console.log(body.toString());
+    });
+});
 
 request.write('Hello World');
 request.end();
