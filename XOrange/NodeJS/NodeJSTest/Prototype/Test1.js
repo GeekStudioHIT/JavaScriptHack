0c642fbd931b1d74c9a99bf43692f4199b8898e7
koinhug@126.com <koinhug@126.com>
2016-08-04 22:15:09 +0800

    状态模式 原型

--- XOrange/NodeJS/NodeJSTest/Prototype/Test1.js
+++ XOrange/NodeJS/NodeJSTest/Prototype/Test1.js
@@ -1,10 +1,69 @@
-function A(name) {
-    this.name = name;
-}
-//noinspection JSAnnotator
-A.prototype.toString() = function () {
-    return this.name;
-};
+// // 继承属性
+// var o = {a: 1, b: 2};
+// o.prototype = {b: 3, c: 4};
+// console.log(o.prototype.b);
 
-var a = new A("hehe");
-console.log(a);
+// // 继承方法
+// var o = {
+//     a: 2,
+//     m: function() {
+//         return this.a + 1;
+//     }
+// };
+//
+// console.log(o.m());
+//
+// var p = Object.create(o);
+//
+// p.a = 12;
+// console.log(p.m());
+
+// // 使用普通语法创建对象
+// var o = {a: 1};
+
+// // 使用构造器创建对象
+// function Graph() {
+//     this.vertexes = [];
+//     this.edges = [];
+// }
+//
+// Graph.prototype = {
+//     addVertex: function (v) {
+//         this.vertexes.push(v);
+//     }
+// };
+//
+// var g = new Graph();
+// console.log(g);
+
+// 使用 Object.create 创建对象
+// var a = {a: 1};
+// var b = Object.create(a);
+// console.log(b.a);
+
+// // class
+// class Polygon {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+// }
+//
+// class Square extends Polygon {
+//     constructor(sideLength) {
+//         super(sideLength, sideLength);
+//     }
+//
+//     get area() {
+//         return this.height * this.width;
+//     }
+//
+//     set sideLength(newLength) {
+//         this.height = newLength;
+//         this.width = newLength;
+//     }
+// }
+//
+// var square = new Square(2);
+// // console.log(square.get());
+//
---
+++ XOrange/Prototype.md
@@ -0,0 +1,5 @@
+# Prototype
+- [MDN](https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Inheritance_and_the_prototype_chain)
+- 每个对象都有一个指向它的原型（prototype）对象的内部链接，这个原型对象又有自己的原型，直到某个对象的原型为 null 为止（也就是不再有原型指向）。原型链（prototype chain）。
+- [豆瓣](https://www.douban.com/note/293217333/)
+- JS 所有的
