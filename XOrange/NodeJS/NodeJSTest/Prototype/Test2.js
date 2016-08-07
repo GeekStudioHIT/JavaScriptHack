aa078c906283ec6408612624dc483c0cbf52ba1d
koinhug@126.com <koinhug@126.com>
2016-08-07 15:23:08 +0800

    构造函数继承总结 我已烂熟于心

--- XOrange/NodeJS/NodeJSTest/Prototype/Test2.js
+++ XOrange/NodeJS/NodeJSTest/Prototype/Test2.js
@@ -1,13 +1,40 @@
-function Animal() {
-    this.species = "动物";
-}
+// function Animal() {
+//     this.species = "动物";
+// }
+//
+// function Cat(name) {
+//     this.name = name;
+// }
+//
+// Cat.prototype = new Animal();
+// // console.log(Cat.prototype);
+// Cat.prototype.constructor = Cat;
+// var cat1 = new Cat("Jemmy");
+// console.log(cat1);
 
-function Cat(name) {
+// function Animal() {}
+// Animal.prototype.species = "动物";
+//
+// function Cat(name) {
+//     this.name = name;
+// }
+//
+// Cat.prototype = Animal.prototype;
+// console.log(Cat.prototype);
+// Cat.prototype.constructor = Cat;
+
+function Animal(name) {
     this.name = name;
 }
+Animal.prototype.species = "动物";
 
-Cat.prototype = new Animal();
-// console.log(Cat.prototype);
+var cat = Animal("cat");
+
+function Cat(obj) {
+    this.obj = obj;
+}
+
+Cat.prototype = Animal.prototype;
 Cat.prototype.constructor = Cat;
-var cat1 = new Cat("Jemmy");
-console.log(cat1);
+var cat1 = new Cat("lalala");
+console.log(cat1.obj + " " + cat1.name);
--- XOrange/Prototype.md
+++ XOrange/Prototype.md
@@ -11,6 +11,12 @@
 	- 直接继承 prototype
 	- 利用空对象作为中介
 	- 拷贝继承
+	- JavaScript 构造函数继承方式总结：
+		1. 最简单的方式，直接将父对象构造函数绑定到子对象的构造函数上，这样父对象里的内容也是子对象的内容，从而实现了继承。
+		2. 将子对象的 prototype 对象指向一个父对象的 new 实例，然后记得让 子对象的 prototype 对象的 constructor 指向 子对象的构造方法（不这么做会造成继承链的紊乱，通俗一点就是，儿子继承了他爸，但他会以为自己是爸爸。）。但这种继承，子对象中所谓继承父对象而来的那些属性，只不过是在 子对象上的 prototype 上。
+		3. 在第 2 种方法的基础上进行改进，直接将子对象的 prototype 对象指向父对象的 prototype 对象，这样的优点是节省内存（节省了生成的父对象的那个实例）。但是缺点是对子对象的 prototype 的修改就是对父对象的 prototype 的修改，并且无法集成父对象中那些可以变的属性。
+		4. 利用空对象作为中介。这是在第 3 种方式的基础上，在子对象的 prototype 和 父对象的 prototype 中间，加一层 空对象。
+		5. 纯粹拷贝继承。将父对象的所有不变属性和方法，拷贝进子对象。
 - [阮一峰 JavaScript 面向对象编程 非构造函数的继承](http://www.ruanyifeng.com/blog/2010/05/object-oriented_javascript_inheritance_continued.html)
 	- object() 方法
 	- 浅拷贝
